da13b4d5bf9d01ffea3b36e9d8a0e47b0ab68bcb
Sardar_Atabekov <sardarbekatabekov@gmail.com>
2025-06-20 21:26:40 +0600

    update render

--- server/index.ts
+++ server/index.ts
@@ -59,18 +59,10 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
-  // server.listen(
-  //   {
-  //     port,
-  //     host: "127.0.0.1",
-  //     reusePort: true,
-  //   },
-  //   () => {
-  //     log(`serving on port ${port}`);
-  //   }
-  // );
-  app.listen(5000, "127.0.0.1", () => {
-    log(`serving on port ${port}`);
+  const port = process.env.PORT ? Number(process.env.PORT) : 5000;
+  const host = process.env.HOST || "0.0.0.0";
+
+  app.listen(port, host, () => {
+    log(`serving on http://${host}:${port}`);
   });
 })();
